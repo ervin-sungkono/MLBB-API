a43c11a25e141b62081e7cb45105bfbbe2cf1a71
ervin-sungkono <ervinsung@gmail.com>
2024-10-18 15:38:56 +0700

    add vercel/blob for storing JSON

--- app/api/heroes/cron/route.js
+++ app/api/heroes/cron/route.js
@@ -1,12 +1,18 @@
 import { fetchListHeroes } from "@/lib/scraper";
 import { NextResponse } from "next/server";
+import { put } from "@vercel/blob";
+
+
 
 export async function GET(){
     try{
-        const blob = await fetchListHeroes()
-        return NextResponse.json({data: blob}, {status: 200})
+        await fetchListHeroes()
     }catch(err){
         console.log(err)
         return NextResponse.json({success: false}, {status: 500})
     }
+
+    
+
+    return NextResponse.json({success: true}, {status: 200})
 }
