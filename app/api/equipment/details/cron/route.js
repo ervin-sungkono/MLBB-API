c697758b7418005e994a169a322e4f8f019ae543
ervin-sungkono <ervinsung@gmail.com>
2024-10-25 21:10:56 +0700

    adjust code

--- app/api/equipment/details/cron/route.js
+++ app/api/equipment/details/cron/route.js
@@ -1,28 +1,15 @@
+import { fetchHeroDetails } from "@/lib/scraper";
 import { NextResponse } from "next/server";
-import { extractOffset } from "@/lib/helper";
-import { head } from "@vercel/blob";
-import fetch from "node-fetch";
-import { SCRAPING_LIMIT } from "@/lib/const";
 
 export const maxDuration = 60;
 
-export async function GET(){
+export async function GET(req){
+    const offset = req.nextUrl.searchParams.get("offset")
+    const limit = req.nextUrl.searchParams.get("limit")
+    
     try{
-        const {url} = await head("output/equipment.json")
-        const equipmentList = await fetch(url).then(res => res.json())
-        const offsets = extractOffset(equipmentList.length)
-        
-        const results = await Promise.all(offsets.map(async(offset) => {
-            const payload = { offset: offset, limit: SCRAPING_LIMIT }
-            const res = await fetch(`https://${process.env.VERCEL_URL}/api/equipment/details`, {
-                method: 'POST',
-                body: JSON.stringify(payload)
-            }).then(res => res.json())
-            console.log("res: ",res)
-            return res
-        }))
-
-        return NextResponse.json({data: results, success: true}, {status: 200})
+        const result = await fetchHeroDetails(offset, limit)
+        return NextResponse.json({data: result, success: true}, {status: 200})
     }catch(err){
         console.log(err)
         return NextResponse.json({success: false}, {status: 500})
