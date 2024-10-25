6c19f3fb1291045d5b78dcaca4dc446f8b788b5d
ervin-sungkono <ervinsung@gmail.com>
2024-10-25 22:23:20 +0700

    add cron for testing

---
+++ app/api/cron/route.js
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import fetch from "node-fetch";
+
+export const maxDuration = 60;
+const baseUrl = 'https://' + process.env.VERCEL_URL
+
+export async function GET(){
+    try{
+        const urls = [`${baseUrl}/api/heroes/cron`, `${baseUrl}/api/equipment/cron`]
+        console.log(urls)
+        const result = await Promise.all(urls.map(url => fetch(url, { 
+            method: 'GET', 
+            headers: {
+                'Content-Type': "application/json"
+            } 
+        })))
+        return NextResponse.json({data: result, success: true}, {status: 200})
+    }catch(err){
+        console.log(err)
+        return NextResponse.json({success: false}, {status: 500})
+    }
+}
