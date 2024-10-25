3fb93b0113707014d9f77dd3bfa84667d983e295
ervin-sungkono <ervinsung@gmail.com>
2024-10-25 16:40:01 +0700

    adjust query params handling

--- app/api/equipment/details/cron/route.js
+++ app/api/equipment/details/cron/route.js
@@ -4,7 +4,9 @@ import { NextResponse } from "next/server";
 export const maxDuration = 60;
 
 export async function GET(req){
-    const { offset, limit } = req.nextUrl.searchParams
+    const offset = req.nextUrl.searchParams.get("offset")
+    const limit = req.nextUrl.searchParams.get("limit")
+    
     try{
         await fetchEquipmentDetails(offset, limit)
         return NextResponse.json({success: true}, {status: 200})
