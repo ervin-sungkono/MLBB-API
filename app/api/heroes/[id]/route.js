29bceb9a27ef7dfb0d9e2362d7f8108c7ccef7c8
ervin-sungkono <ervinsung@gmail.com>
2024-11-02 20:43:13 +0700

    add API rate-limit using LRU cache

--- app/api/heroes/[id]/route.js
+++ app/api/heroes/[id]/route.js
@@ -1,14 +1,30 @@
 import { head } from "@vercel/blob";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
+import { isRateLimited } from "@/lib/rate-limit";
+import { MAX_REQUESTS_PER_INTERVAL } from "@/lib/const";
 import fetch from "node-fetch";
 
 export async function GET(request, context){
+    const { id } = context.params
+    const headerList = headers()
+    const cacheToken = `id-${headerList["x-forwarded-for"] || headerList.socket?.remoteAddress || "unknown"}`;
+
+    const { limited, remaining } = isRateLimited(cacheToken);
+    if(limited){
+        return NextResponse.json({ error: "Rate limit exceeded" }, {status: 429})
+    }
+
     try{
-        const { id } = context.params
         const { url } = await head(`hero/${id}.json`)
 
         const data = await fetch(url).then(res => res.json())
-        return NextResponse.json({data: data, success: true}, {status: 200})
+        return NextResponse.json({ 
+            ratelimit_limit: MAX_REQUESTS_PER_INTERVAL,
+            ratelimit_remaining: remaining,
+            success: true,
+            data: data
+        }, {status: 200})
     }catch(err){
         console.log(err)
         return NextResponse.json({data:null, success: false}, {status: 404})
